30d723762f5aa26dbc2593e57098fc050baec15d
Novaras <tfear091@gmail.com>
2021-05-26 17:55:55 +0100

    update homeworld types & constants

--- src/homeworld.ts
+++ src/homeworld.ts
@@ -9,7 +9,7 @@ export type ActivationContext = typeof ActivationContexts[number];
 export const ShieldPenetrationModes = <const>[`normal`, `enhanced`,  `bypass`];
 export type ShieldPenetrationMode = typeof ShieldPenetrationModes[number];
 
-declare interface Weapon {
+export interface Weapon {
 	name: string,
 	display_name: string,
 	weapon_type: WeaponType,
@@ -39,82 +39,93 @@ declare interface Weapon {
 	beam_penetration_threshold: number,
 }
 
-declare interface AttackFamilyExceptions {
-	fighter: number,
-	fighter_hw1: number,
-	corvette: number,
-	corvette_hw1: number,
-	frigate: number,
-	utility: number,
-	muniton: number,
-	smallcapitalship: number,
-	bigcapitalship: number,
-	mothership: number,
-	emplacement: number,
-	unattackable: number,
-	subsystem: number,
-	resourcelarge: number,
-	capturer: number,
-	chimera: number,
-	nonsensefamily: number,
-	noncombat: number,
-	swarmer: number,
-	spacemine: number,
-	torpedo: number,
-	heavymissile: number,
-	smallmissile: number,
+export const AttackFamilies = [
+	`fighter`,
+	`fighter_hw1`,
+	`corvette`,
+	`corvette_hw1`,
+	`frigate`,
+	`utility`,
+	`munition`,
+	`smallcapitalship`,
+	`bigcapitalship`,
+	`mothership`,
+	`emplacement`,
+	`unattackable`,
+	`subsystem`,
+	`resourcelarge`,
+	`capturer`,
+	`nonsensefamily`,
+	`noncombat`,
+	`swarmer`,
+	`spacemine`,
+	`torpedo`,
+	`heavymissile`,
+	`smallmissile`
+] as const;
+
+export type AttackFamilyExceptions = {
+	[k in typeof AttackFamilies[number]]?: number
+};
+
+export const ArmourFamilies = [
+	`unarmored`,
+	`unarmoured_hw1`,
+	`lightarmour`,
+	`lightarmour_hw1`,
+	`mediumarmour`,
+	`heavyarmour`,
+	`subsystemarmour`,
+	`turretarmour`,
+	`resarmour`,
+	`moverarmour`,
+	`planetkillerarmour`,
+	`minearmour`,
+	`chunkarmour`,
+	`resourcearmour`,
+	`gravitywellarmour`,
+	`swarmerarmour`,
+	`spaceminearmour`,
+	`torpedoarmour`,
+	`heavymissilearmour`,
+	`smallmissilearmour`,
+	`probearmour`
+] as const;
+
+export type ArmourFamilyExceptions = {
+	[k in typeof ArmourFamilies[number]]?: number
 }
 
-declare interface ArmourFamilyExceptions {
-	unarmoured: number,
-	unarmoured_hw1: number,
-	lightarmour: number,
-	lightarmour_hw1: number,
-	mediumarmour: number,
-	mediumarmour_hw1: number,
-	heavyarmour: number,
-	subsystemarmour: number,
-	turretarmour: number,
-	resarmour: number,
-	moverarmour: number,
-	planetkillerarmour: number,
-	minearmour: number,
-	chunkarmour: number,
-	resourcearmour: number,
-	gravitywellarmour: number,
-	swarmerarmour: number,
-	spaceminearmour: number,
-	torpedoarmour: number,
-	heavymissilearmour: number,
-	smallmissilearmour: number,
-	probearmour: number
-}
-
-declare interface MagneticFieldExceptions {
-	MagneticField: number,
+export const MagneticFieldFamilies = [`magneticfield`] as const;
+export type MagneticFieldFamilyExceptions = {
+	[k in typeof MagneticFieldFamilies[number]]?: number
 }
 
 // todo: needs a mention that there is no fields for `setDamageMultFactor`, since I have 0 clue what it does if anything
 // same for `setFireMultFactor`
 // same for `setRangeBoost`
-declare interface SpeedVsAccuracy {
+export interface SpeedVsAccuracy {
 	speed: number,
 	accuracy: number
 }
 
-export declare interface WeaponExt extends Weapon {
+export const ResultEffects = [`damagehealth`, `disable`, `push`, `spawnweaponfire`] as const;
+export const ResultConditions = [`hit`, `miss`] as const;
+export const ResultTargets = [`target`, `owner`] as const; 
+
+export interface WeaponExt extends Weapon {
 	turret_sound?: string,
 	weapon_result: {
-		condition: `hit` | `miss`,
-		effect: `damagehealth` | `disable` | `push` | `spawnweaponfire`,
-		target: `target` | `owner`,
+		condition: typeof ResultConditions[number],
+		effect: typeof ResultEffects[number],
+		target: typeof ResultTargets[number],
 		effect_min: number,
 		effect_max: number,
 		spawns_weapon?: string,
 	},
 	accuracy: {
 		default: number,
-		family_exceptions?: AttackFamilyExceptions
+		exceptions?: AttackFamilyExceptions
 	},
 	accuracy_falloff?: {
 		falloff_over_range: number,
@@ -128,9 +139,8 @@ export declare interface WeaponExt extends Weapon {
 		rotate_y_max: number,
 	},
 	ballistics?: {
-		unknown_0: boolean,
-		unknown_1: number,
-		unknown_2: number
+		disabled: boolean,
+		lead_amount: number
 	},
 	damage_falloff?: {
 		falloff_over_range: number,
@@ -143,7 +153,7 @@ export declare interface WeaponExt extends Weapon {
 	lifetime_mult?: number,
 	magnetic_field_pen?: {
 		default: number,
-		family_exceptions?: MagneticFieldExceptions
+		exceptions?: MagneticFieldFamilyExceptions
 	},
 	misc_values?: {
 		barrel_recoil_distance: number,
@@ -151,8 +161,8 @@ export declare interface WeaponExt extends Weapon {
 	},
 	missile_killer?: boolean,
 	miss_properties?: {
-		cone_x: number,
-		cone_y: number,
+		cone_x_max: number,
+		cone_y_max: number,
 		damage_mult_low: number,
 		damage_mult_high: number,
 		projectile_speed_mult: number,
@@ -160,7 +170,7 @@ export declare interface WeaponExt extends Weapon {
 	},
 	penetration: {
 		default: number,
-		family_exceptions?: ArmourFamilyExceptions
+		exceptions?: ArmourFamilyExceptions
 	},
 	range_boost?: number,
 	stance_ranges?: {
@@ -180,3 +190,5 @@ export declare interface WeaponExt extends Weapon {
 		],
 	}
 }
+
+export type AnyEnumerable = (typeof WeaponTypes | typeof ProjectileTypes | typeof ShieldPenetrationModes | typeof ActivationContexts | typeof AttackFamilies | typeof ArmourFamilies);
